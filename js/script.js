bf84164ff03dd1a15d8a464b04439cb5a561ab01
starfall101 <184286666+starfall218@users.noreply.github.com>
2025-04-22 21:35:27 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 const anotherClass = document.querySelectorAll('.another');
+const box = document.querySelector('.box');
 anotherClass.forEach(function (elem) {
   console.log(elem);
-  elem.style.display = 'block';
-  elem.style.textAlign = 'center';
+  box.style.height = '100vh';
+  box.style.display = 'flex';
+  box.style.flexDirection = 'column';
+  box.style.alignItems = 'center';
+  box.style.justifyContent = 'center';
   if (elem.innerHTML.includes('here')) {
     elem.style.backgroundColor = 'blue';
+    elem.style.textAlign = 'center';
+    elem.style.width = '100%';
     elem.style.color = '#fff';
-  }
+  };
 });
